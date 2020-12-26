bd7604de59fe0b9c226e98fd20bca7df94fa5a22
wusung <twkoci@gmail.com>
2020-12-26 14:15:07 +0800

    remove gnupg2

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt install gnupg2
 wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
 sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 sudo percona-release setup ps80
